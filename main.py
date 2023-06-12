af8fa9feed30958f635580704ca430e3310d1c36
Dentril <62445497+Dentril@users.noreply.github.com>
2023-06-12 20:54:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return "Ola mundo"
+def homepage():
+    return "OLA MUNDO!"
+
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=False, port=os.getenv("PORT", default=5000))
+## app.run(debug=True, port=os.getenv("PORT", default=5000))
